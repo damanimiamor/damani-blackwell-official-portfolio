2da7061ac877143c7d8a3abae528008cc1a3ce79
damanimiamor <79300272+damanimiamor@users.noreply.github.com>
2023-06-17 05:16:18 -0400

    Add files via upload
    Adding javascript

---
+++ Javascript-files/portfolio.js
@@ -0,0 +1,9 @@
+//addressing footer issue rendering in view during transition on index page//
+let footer = document.getElementById('index-footer');
+
+setTimeout(() => {
+    footer.classList.remove('hidden');
+}, 3000);
+
+
+
