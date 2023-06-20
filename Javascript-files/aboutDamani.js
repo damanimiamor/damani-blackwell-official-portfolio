25146480d651e5d0e8935331e385983e4665a00c
damanimiamor <79300272+damanimiamor@users.noreply.github.com>
2023-06-20 04:16:17 -0400

    Update aboutDamani.js

--- Javascript-files/aboutDamani.js
+++ Javascript-files/aboutDamani.js
@@ -10,7 +10,7 @@ formIcon.addEventListener('click',() => {
         contactForm = document.createElement('div');
         contactForm.classList.add('contact-form');
 
-        contactForm.innerHTML = '<form id="contact-form" name="contact-form" netlify method="post">' +
+        contactForm.innerHTML = '<form id="contact-form" name="contact-form" data-netlify="true" method="post">' +
         '<div id="text-container">' +
         '<div id="name-container">' +
         '<label for="name" class="form-label"> Please enter your name:</label>' +
